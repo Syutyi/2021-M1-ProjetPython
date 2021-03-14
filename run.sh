90bacb6faa5c80df36e441359100dbe5eb1ddd60
Marin Blot <marin.blot@edu.devinci.fr>
2021-03-14 18:41:56 +0100

    Adding the exercise 4 with the bash script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d "_env"]
+then
+	echo "The virtual environnement doest not exist yet"
+	python3 -m venv _env
+fi
+echo "The Python virtual envrionnement exists now, let's activate it"
+source _env/bin/activate
+echo "The virtual environnement has been activated, running the python code ..."
+
+python main.py
+
+echo "Process successfully completed !"
