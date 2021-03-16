eb3d42ece846829fc400b25bcf0dd231c8890726
Syutyi <syutyi.cheng@gmail.com>
2021-03-16 16:39:05 +0100

    Correcting the bash

--- run.sh
+++ run.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
-if [ ! -d "_env" ]
+if [ ! -d "_env" ] 
 then
-	echo "The virtual environnement doest not exist yet"
+	echo "The python environnement does not exist yet"
 	python3 -m venv _env
 fi
 echo "The Python virtual envrionnement exists now, let's activate it"
 source _env/bin/activate
 echo "Loading the requirements"
 pip install -r requirements.txt
-
 echo "The virtual environnement has been activated, running the python code ..."
-
 python main.py
-
 echo "Process successfully completed !"
